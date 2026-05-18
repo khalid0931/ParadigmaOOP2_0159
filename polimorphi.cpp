3fa018c83049bedb77ae97e25c7fc5a0365699d6
khalid0931 <mutiarif53@gmail.com>
2026-05-18 14:53:47 +0700

    class derived

--- polimorphi.cpp
+++ polimorphi.cpp
@@ -7,3 +7,10 @@ public:
         cout << "Hallo saya Function dari base class" << endl;
     }
 };
+
+class derivedClass : public baseClass {
+public:
+    void perkenalan() override {
+        cout << "Hallo saya Function dari derived Class" << endl;
+    }
+};
